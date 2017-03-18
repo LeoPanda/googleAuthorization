c298bc7ef8634c4f9e9f4b1d00d1dc5a18e10859
Toru Sakyo <toru.sakyo@gmail.com>
2017-03-18 14:05:30 +0900

    gPlusAnalyticsに適用

--- googleAuthorization/src/jp/leopanda/googleAuthorization/server/CatchException.java
+++ googleAuthorization/src/jp/leopanda/googleAuthorization/server/CatchException.java
@@ -1,15 +1,13 @@
 package jp.leopanda.googleAuthorization.server;
 
-import java.io.IOException;
-
-import org.apache.http.HttpStatus;
-
+import javax.servlet.http.HttpServletResponse;
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 
 import jp.leopanda.googleAuthorization.client.ForbiddenException;
 
 /**
  * Auth認証を利用したAPIコール使用時の共通例外処理
+ * 
  * @author LeoPanda
  *
  */
@@ -17,41 +15,47 @@ public abstract class CatchException<E> {
 
   /**
    * 実装したAPIコールを実行する
+   * 
    * @return E 指定した戻り値
    * @throws Exception
    */
-  public E execute() throws Exception{
+  public E execute() throws Exception {
     E result = null;
-    try{
+    try {
       result = tryApiCall();
-    } catch(Throwable e){
+    } catch (Throwable e) {
       afterProcess(e);
     }
     return result;
   }
-  
+
   /**
    * Auth認証を使用したAPIコールを実装する
+   * 
    * @return
    * @throws Exception
    */
   public abstract E tryApiCall() throws Exception;
-  
+
   /**
    * 認証エラーの場合はストアされた認証を削除し認証例外を発生させる
+   * 
    * @param e
    * @throws Exception
    */
-  private void afterProcess(Throwable e) throws Exception{
+  private void afterProcess(Throwable e) throws Exception {
     if (e instanceof GoogleJsonResponseException) {
       int statusCode = ((GoogleJsonResponseException) e).getStatusCode();
-      if (statusCode == HttpStatus.SC_FORBIDDEN || statusCode == HttpStatus.SC_UNAUTHORIZED) {
+      if (statusCode == HttpServletResponse.SC_FORBIDDEN || statusCode == HttpServletResponse.SC_UNAUTHORIZED) {
         new CredentialUtils().removeCredentialStore();
         throw new ForbiddenException(e);
       } else {
         e.printStackTrace();
         throw new Exception(e);
       }
+    } else {
+      e.printStackTrace();
+      throw new Exception(e);
 
     }
   }
--- googleAuthorization/src/jp/leopanda/googleAuthorization/server/CredentialUtils.java
+++ googleAuthorization/src/jp/leopanda/googleAuthorization/server/CredentialUtils.java
@@ -4,12 +4,16 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+import java.util.Date;
 
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.appengine.datastore.AppEngineDataStoreFactory;
 import com.google.api.client.extensions.appengine.http.UrlFetchTransport;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.http.GenericUrl;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
@@ -17,6 +21,8 @@ import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserServiceFactory;
 import com.google.appengine.api.utils.SystemProperty;
+
+import jp.leopanda.googleAuthorization.client.NoCredentialException;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -28,23 +34,72 @@ import javax.servlet.http.HttpServletRequest;
  */
 public class CredentialUtils {
 
-  public final HttpTransport HTTP_TRANSPORT = new UrlFetchTransport();
-  public final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+  public final HttpTransport httpTransport = new UrlFetchTransport();
+  public final JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
 
   private final String callbackPath = "/oauth2callback";
+  private final String clientSecretFilePath = "WEB-INF/client_secrets.json";
 
   /**
-   * Google App Engine クライアントシークレットの生成
+   * 認証オブジェクトをロードする
    * 
+   * @return 認証オブジェクト
+   * @throws IOException
+   * @throws NoCredentialException 
+   */
+  public Credential loadCredential() throws IOException, NoCredentialException {
+    Credential credential = getFlow()
+        .loadCredential(getCurrentUserId());
+    return checkExpired(credential);
+  }
+
+  /**
+   * 認証オブジェクトの有効期限をチェックし、切れていればデータストアから削除する
+   * 
+   * @param credential
    * @return
    * @throws IOException
+   * @throws NoCredentialException 
    */
-  public GoogleClientSecrets getClientSecrets() throws IOException {
-    if (Statics.clientSecrets == null) {
-      Statics.clientSecrets = GoogleClientSecrets.load(JSON_FACTORY,
-          new InputStreamReader(new FileInputStream(new File("WEB-INF/client_secrets.json"))));
+  private Credential checkExpired(Credential credential) throws IOException, NoCredentialException {
+    if (credential == null) {
+      return null;
     }
-    return Statics.clientSecrets;
+    Long expirationTIme = credential.getExpirationTimeMilliseconds();
+    if (expirationTIme == null) {
+      return credential;
+    }
+    Long nowTime = new Date().getTime();
+    if (expirationTIme - nowTime < 10 * 1000) {
+      removeCredentialStore();
+      throw new NoCredentialException(getAuthUrl());
+    } else {
+      return credential;
+    }
+  }
+
+  /**
+   * キーファイル認証生成
+   * 
+   * @param p12key
+   *          File 認証キー
+   * @param emailAdress
+   *          String サービスアカウントID
+   * @return GoogleCredential Google認証オブジェクト
+   * @throws GeneralSecurityException
+   *           認証れ～概
+   * @throws IOException
+   *           IO例外
+   */
+  public GoogleCredential getCredentialByP12(File p12key, String emailAdress, String scope)
+      throws GeneralSecurityException, IOException {
+    GoogleCredential credential = new GoogleCredential.Builder().setTransport(httpTransport)
+        .setJsonFactory(jsonFactory)
+        .setServiceAccountId(emailAdress)
+        .setServiceAccountScopes(Collections.singleton(scope))
+        .setServiceAccountPrivateKeyFromP12File(p12key)
+        .build();
+    return credential;
   }
 
   /**
@@ -54,12 +109,54 @@ public class CredentialUtils {
    * @throws IOException
    */
   public GoogleAuthorizationCodeFlow getFlow() throws IOException {
-    GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT,
-        JSON_FACTORY, getClientSecrets(), Statics.scopes)
+    GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(httpTransport,
+        jsonFactory, getClientSecrets(), Statics.scopes)
             .setDataStoreFactory(AppEngineDataStoreFactory
                 .getDefaultInstance())
             .setAccessType("offline").build();
     return flow;
+  }
+
+  /**
+   * Google App Engine クライアントシークレットの生成
+   * 
+   * @return
+   * @throws IOException
+   */
+  private GoogleClientSecrets getClientSecrets() throws IOException {
+    if (Statics.clientSecrets == null) {
+      Statics.clientSecrets = GoogleClientSecrets.load(jsonFactory,
+          new InputStreamReader(new FileInputStream(new File(clientSecretFilePath))));
+    }
+    return Statics.clientSecrets;
+  }
+
+  /**
+   * HTTP Transportを取得する
+   * 
+   * @return
+   */
+  public HttpTransport getTransport() {
+    return this.httpTransport;
+  }
+
+  /**
+   * JsonFactoryを所得する
+   * 
+   * @return
+   */
+  public JsonFactory getJsonFactory() {
+    return this.jsonFactory;
+  }
+
+  /**
+   * カレントのユーザーIDを取得する
+   * 
+   * @return
+   */
+  public String getCurrentUserId() {
+    User user = UserServiceFactory.getUserService().getCurrentUser();
+    return user.getUserId();
   }
 
   /**
@@ -88,18 +185,6 @@ public class CredentialUtils {
   }
 
   /**
-   * 認証オブジェクトをロードする
-   * 
-   * @return 認証オブジェクト
-   * @throws IOException
-   */
-  public Credential loadCredential() throws IOException {
-    Credential credential = getFlow()
-        .loadCredential(getCurrentUserId());
-    return credential;
-  }
-
-  /**
    * 認証取得を申請する
    * 
    * @return 認証完了後のコールバックURL
@@ -111,16 +196,6 @@ public class CredentialUtils {
   }
 
   /**
-   * カレントのユーザーIDを取得する
-   * 
-   * @return
-   */
-  private String getCurrentUserId() {
-    User user = UserServiceFactory.getUserService().getCurrentUser();
-    return user.getUserId();
-  }
-
-  /**
    * コールバックURLを生成する（RPC用）
    * 
    * @return
@@ -128,7 +203,7 @@ public class CredentialUtils {
   private String getCallbackUrl() {
     String callbackUrl;
     if (SystemProperty.environment.get().equals("Development")) {
-      callbackUrl = "http://127.0.0.1:8888" + callbackPath;
+      callbackUrl = "http://localhost:8080" + callbackPath;
     } else {
       String version = SystemProperty.applicationVersion.get().split("\\.")[0];
       String appName = SystemProperty.applicationId.get();
